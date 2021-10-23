dedc2b056549cd647de3cdfa9e1e871dd5b696aa
Farhana P Salim <69158399+farhanapsalim@users.noreply.github.com>
2021-10-23 16:26:16 +0530

    Update Bookdata.js

--- src/model/Bookdata.js
+++ src/model/Bookdata.js
@@ -3,8 +3,8 @@
 const mongoose=require('mongoose');
 
 //database connection
-mongoose.connect('mongodb://localhost:27017/library');
-//mongoose.connect('mongodb+srv://userone:userone@ictakfiles.kj5za.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
+//mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb+srv://userone:userone@ictakfiles.kj5za.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 //mongodb+srv://userone:<password>@ictakfiles.kj5za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 //schema definition
